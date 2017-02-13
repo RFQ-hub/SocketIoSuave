a5b30f2642f46012eff962b905d158575dabea0c
Julien Roncaglia <julien.roncaglia@itg.com>
2017-02-13 13:30:17 +0100

    Extract engine code

---
+++ SocketIoSuave/Choice.fs
@@ -0,0 +1,12 @@
+﻿module SocketIoSuave.Choice
+
+open System
+
+/// Return the value if it's a Choice1Of2 or default' otherwise
+let defaultArg default' = function | Choice1Of2 x -> x | Choice2Of2 _ -> default'
+
+let parseInt s =
+    let ok, i = Int32.TryParse(s)
+    if ok then Choice1Of2 i else Choice2Of2 (sprintf "Not a number: '%s'" s)
+
+let parseIntAsBool s = parseInt s |> Choice.map((<>) 0)
